5503f7337f559a1f539ab13f480f5e3a1f67efba
arifkhan <investkhan7@gmail.com>
2019-08-27 00:09:27 +0530

    added user role in token response.

--- waste-management-system/src/main/java/com/wms/api/controller/AuthenticationController.java
+++ waste-management-system/src/main/java/com/wms/api/controller/AuthenticationController.java
@@ -1,4 +1,7 @@
 package com.wms.api.controller;
+
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,30 +19,30 @@ import com.wms.api.config.TokenProvider;
 import com.wms.api.dto.AuthToken;
 import com.wms.api.dto.LoginUser;
 
+import io.jsonwebtoken.Claims;
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/token")
 public class AuthenticationController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private TokenProvider jwtTokenUtil;
+	@Autowired
+	private TokenProvider jwtTokenUtil;
 
+	@RequestMapping(value = "/generate-token", method = RequestMethod.POST)
+	public ResponseEntity<?> register(@RequestBody LoginUser loginUser) throws AuthenticationException {
 
-    @RequestMapping(value = "/generate-token", method = RequestMethod.POST)
-    public ResponseEntity<?> register(@RequestBody LoginUser loginUser) throws AuthenticationException {
-
-        final Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginUser.getUsername(),
-                        loginUser.getPassword()
-                )
-        );
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        final String token = jwtTokenUtil.generateToken(authentication);
-        return ResponseEntity.ok(new AuthToken(token));
-    }
-
+		final Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginUser.getUsername(), loginUser.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		final String token = jwtTokenUtil.generateToken(authentication);
+		final Date expiryDate = jwtTokenUtil.getExpirationDateFromToken(token);
+		final Claims claims = jwtTokenUtil.getAllClaimsFromToken(token);
+		final String userRole= (String) claims.get("scopes");
+		
+		return ResponseEntity.ok(new AuthToken(token, expiryDate, userRole));
+	}
 }
--- waste-management-system/src/main/java/com/wms/api/dto/AuthToken.java
+++ waste-management-system/src/main/java/com/wms/api/dto/AuthToken.java
@@ -1,15 +1,37 @@
 package com.wms.api.dto;
 
+import java.util.Date;
+
 public class AuthToken {
 
 	private String token;
+	private Date tokenExpiryDate;
+	private String userRole;
 
 	public AuthToken() {
 
 	}
 
-	public AuthToken(String token) {
+	public AuthToken(String token, Date tokenExpiryDate, String subject) {
 		this.token = token;
+		this.tokenExpiryDate = tokenExpiryDate;
+		this.userRole = subject;
+	}
+
+	public String getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+
+	public Date getTokenExpiryDate() {
+		return tokenExpiryDate;
+	}
+
+	public void setTokenExpiryDate(Date tokenExpiryDate) {
+		this.tokenExpiryDate = tokenExpiryDate;
 	}
 
 	public String getToken() {
@@ -19,5 +41,4 @@ public class AuthToken {
 	public void setToken(String token) {
 		this.token = token;
 	}
-
 }
