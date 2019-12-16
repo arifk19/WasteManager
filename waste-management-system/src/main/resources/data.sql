6e2a59a1c14a45889042e49137cd41f9bea55ac5
Khan <mkhan339@dxc.com>
2019-12-16 18:07:47 +0530

    added database to auto creation of the tables

---
+++ waste-management-system/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO `wastemanagement`.`role` (`id`,`description`, `name`) VALUES (1,'admin role','ADMIN');
+INSERT INTO `wastemanagement`.`role` (`id`,`description`, `name`) VALUES (2,'user role','USER');
